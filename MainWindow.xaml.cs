b84086d05f97a0e4875810e243740ace8f193379
0ne_Up <oneup1@gmail.com>
2020-05-31 13:22:49 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Encryptor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly byte[] ENCRYPTION_SALT = new byte[] {
+            0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 };
+       
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            String s = Encrypt("Hello World", "fubar");
+            Console.WriteLine(s);
+
+            Console.WriteLine(Decrypt(s, "fubar"));
+        }
+
+        private static string Encrypt(String s, String password)
+        {
+            byte[] bytes = Encoding.Unicode.GetBytes(s);
+            using (Aes aes = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(password, ENCRYPTION_SALT);
+                aes.Key = pdb.GetBytes(32);
+                aes.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms,
+                        aes.CreateEncryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytes, 0, bytes.Length);
+                        cs.Close();
+                    }
+                    return Convert.ToBase64String(ms.ToArray());
+                }
+            }
+        }
+
+        private static string Decrypt(String s, String password)
+        {
+            s = s.Replace(" ", "+");
+            byte[] bytes = Convert.FromBase64String(s);
+            using (Aes aes = Aes.Create())
+            {
+                Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(password, ENCRYPTION_SALT);
+                aes.Key = pdb.GetBytes(32);
+                aes.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms,
+                        aes.CreateDecryptor(), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytes, 0, bytes.Length);
+                        cs.Close();
+                    }
+                    return Encoding.Unicode.GetString(ms.ToArray());
+                }
+            }
+        }
+    }
+}
